e72767ae419acf54e3b84ed84f1ab9b95b1534f9
VaclavHaramule <xerorbattler@gmail.com>
2011-12-08 10:25:36 +0100

    BlackJack cloned for Bukkit version

--- pro4/dist/run.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-echo Spoustim program Asistentky, PRO ukol 4
-java -jar pro4.jar
-echo Program Asistentky ukoncen, vytvoril Vaclav Haramule
-PAUSE
--- pro5/dist/run.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-echo Spoustim program Zlodej, PRO ukol 5
-java -jar pro5.jar
-echo Program Zlodej ukoncen, vytvoril Vaclav Haramule
-PAUSE
